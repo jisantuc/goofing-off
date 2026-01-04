87ede0ca869f059962bca8d25a68d1c0d4f938d6
James Santucci <james.santucci@gmail.com>
2026-01-03 22:56:07 -0800

    WIP -- sim runs woohoo just gotta wire up CLI / output

--- src/mosconi-sim/Match.hs
+++ src/mosconi-sim/Match.hs
@@ -1,12 +1,16 @@
-{-# LANGUAGE DataKinds #-}
-
 module Match
-  ( Schedule,
+  ( Player (..),
+    Schedule,
+    DoublesTeam (..),
     Team (..),
+    Matchup (..),
     makeSchedule,
+    scheduleToList,
+    teamToList,
   )
 where
 
+import Control.Monad.Trans.State.Lazy (State, state)
 import Data.Text (Text)
 import System.Random.Stateful (RandomGen, randomR)
 
@@ -14,28 +18,28 @@ data Player = Player
   { name :: Text,
     rating :: Int
   }
-  deriving (Eq, Show)
+  deriving (Eq, Ord, Show)
 
 data Team = Team Player Player Player Player Player deriving (Eq, Show)
 
-toList :: Team -> [Player]
-toList (Team p1 p2 p3 p4 p5) = [p1, p2, p3, p4, p5]
+teamToList :: Team -> [Player]
+teamToList (Team p1 p2 p3 p4 p5) = [p1, p2, p3, p4, p5]
 
 data DoublesTeam = DoublesTeam Player Player deriving (Show)
 
-newtype SinglesMatchup = SinglesMatchup (Player, Player) deriving (Show)
-
-newtype DoublesMatchup = DoublesMatchup (DoublesTeam, DoublesTeam) deriving (Show)
-
-newtype TeamMatchup = TeamMatchup (Team, Team) deriving (Eq, Show)
+data Matchup
+  = SinglesMatchup (Player, Player)
+  | DoublesMatchup (DoublesTeam, DoublesTeam)
+  | TeamMatchup (Team, Team)
+  deriving (Show)
 
 -- All players must play at least once in matches 2, 3, and 4.
 data Day1
   = Day1
-      TeamMatchup
-      DoublesMatchup
-      SinglesMatchup
-      DoublesMatchup
+      Matchup
+      Matchup
+      Matchup
+      Matchup
   deriving (Show)
 
 -- All players must play at least once in matches 7, 8, and 9.
@@ -44,23 +48,23 @@ data Day1
 -- different from day 1, so I'll include that constraint as well
 data Day2
   = Day2
-      TeamMatchup
-      SinglesMatchup
-      DoublesMatchup
-      SinglesMatchup
-      DoublesMatchup
+      Matchup
+      Matchup
+      Matchup
+      Matchup
+      Matchup
   deriving (Show)
 
 -- All players must play at least once in matches 12, 13, and 14.
 -- that's the third/fourth/fifth match of the day
 data Day3
   = Day3
-      TeamMatchup
-      SinglesMatchup
-      DoublesMatchup
-      SinglesMatchup
-      DoublesMatchup
-      SinglesMatchup
+      Matchup
+      Matchup
+      Matchup
+      Matchup
+      Matchup
+      Matchup
   deriving (Show)
 
 -- |
@@ -69,12 +73,12 @@ data Day3
 -- player has played once, but really, that's just a guess.
 data Day4
   = Day4
-      SinglesMatchup
-      SinglesMatchup
-      SinglesMatchup
-      SinglesMatchup
-      SinglesMatchup
-      SinglesMatchup
+      Matchup
+      Matchup
+      Matchup
+      Matchup
+      Matchup
+      Matchup
   deriving (Show)
 
 -- |
@@ -90,9 +94,9 @@ data Schedule
       Day3
       Day4
 
-shuffleTeam :: (RandomGen g) => g -> Team -> (Team, g)
-shuffleTeam gen team =
-  go gen [] (toList team)
+shuffleTeam :: (RandomGen g) => Team -> State g Team
+shuffleTeam team = state $ \gen ->
+  go gen [] (teamToList team)
   where
     go g [p1', p2', p3', p4', p5'] _ = (Team p1' p2' p3' p4' p5', g)
     go g acc rest =
@@ -101,71 +105,101 @@ shuffleTeam gen team =
           selected = rest !! idx
        in go g' (acc <> [selected]) (filter (/= selected) rest)
 
-singlesMatchup :: (RandomGen g) => g -> Team -> Team -> (SinglesMatchup, g)
-singlesMatchup gen teamA teamB =
+singlesMatchup :: (RandomGen g) => Team -> Team -> State g Matchup
+singlesMatchup teamA teamB = state $ \gen ->
   let (idx1, g') = randomR (0, 4) gen
       (idx2, g'') = randomR (0, 4) g'
-   in (SinglesMatchup (toList teamA !! idx1, toList teamB !! idx2), g'')
+   in (SinglesMatchup (teamToList teamA !! idx1, teamToList teamB !! idx2), g'')
 
 -- |
 --  In principle this doesn't have to go doubles / singles / doubles, but in the rules that's the order that
 --  the constraint always shows up in 🤷
-exhaustiveMatchSeq :: (RandomGen g) => g -> Team -> Team -> ((DoublesMatchup, SinglesMatchup, DoublesMatchup), g)
-exhaustiveMatchSeq gen teamA teamB =
-  let (Team a1' a2' a3' a4' a5', g') = shuffleTeam gen teamA
-      (Team b1' b2' b3' b4' b5', g'') = shuffleTeam g' teamB
-   in ( ( DoublesMatchup (DoublesTeam a1' a2', DoublesTeam b1' b2'),
-          SinglesMatchup (a3', b3'),
-          DoublesMatchup ((DoublesTeam a4' a5'), (DoublesTeam b4' b5'))
-        ),
-        g''
+exhaustiveMatchSeq :: (RandomGen g) => Team -> Team -> State g (Matchup, Matchup, Matchup)
+exhaustiveMatchSeq teamA teamB =
+  do
+    (Team a1' a2' a3' a4' a5') <- shuffleTeam teamA
+    (Team b1' b2' b3' b4' b5') <- shuffleTeam teamB
+    pure
+      ( DoublesMatchup (DoublesTeam a1' a2', DoublesTeam b1' b2'),
+        SinglesMatchup (a3', b3'),
+        DoublesMatchup ((DoublesTeam a4' a5'), (DoublesTeam b4' b5'))
       )
 
-makeDay1 :: (RandomGen g) => g -> Team -> Team -> (Day1, g)
-makeDay1 gen teamA teamB =
-  let (teamMatchOrderA, g') = shuffleTeam gen teamA
-      (teamMatchOrderB, g'') = shuffleTeam g' teamB
-      ((doubles1, singles, doubles2), g''') = exhaustiveMatchSeq g'' teamA teamB
-   in (Day1 (TeamMatchup (teamMatchOrderA, teamMatchOrderB)) doubles1 singles doubles2, g''')
+makeDay1 :: (RandomGen g) => Team -> Team -> State g Day1
+makeDay1 teamA teamB = do
+  teamMatchOrderA <- shuffleTeam teamA
+  teamMatchOrderB <- shuffleTeam teamB
+  (doubles1, singles, doubles2) <- exhaustiveMatchSeq teamA teamB
+  pure $ Day1 (TeamMatchup (teamMatchOrderA, teamMatchOrderB)) doubles1 singles doubles2
 
-makeDay2 :: (RandomGen g) => g -> Team -> Team -> (Day2, g)
-makeDay2 gen teamA teamB =
-  let (shuffledA, g') = shuffleTeam gen teamA
-      (shuffledB, g'') = shuffleTeam g' teamB
-      teamMatch = TeamMatchup (shuffledA, shuffledB)
-      (singles1, g''') = singlesMatchup g'' teamA teamB
-      ((doubles1, singles2, doubles2), g'''') = exhaustiveMatchSeq g''' teamA teamB
-   in (Day2 teamMatch singles1 doubles1 singles2 doubles2, g'''')
+makeDay2 :: (RandomGen g) => Team -> Team -> State g Day2
+makeDay2 teamA teamB = do
+  shuffledA <- shuffleTeam teamA
+  shuffledB <- shuffleTeam teamB
+  let teamMatch = TeamMatchup (shuffledA, shuffledB)
+  singles1 <- singlesMatchup teamA teamB
+  (doubles1, singles2, doubles2) <- exhaustiveMatchSeq teamA teamB
+  pure $ Day2 teamMatch singles1 doubles1 singles2 doubles2
 
-makeDay3 :: (RandomGen g) => g -> Team -> Team -> (Day3, g)
-makeDay3 gen teamA teamB =
-  let (shuffledA, g') = shuffleTeam gen teamA
-      (shuffledB, g'') = shuffleTeam g' teamB
-      teamMatch = TeamMatchup (shuffledA, shuffledB)
-      (singles1, g''') = singlesMatchup g'' teamA teamB
-      ((doubles1, singles2, doubles2), g'''') = exhaustiveMatchSeq g''' teamA teamB
-      (singles3, g''''') = singlesMatchup g'''' teamA teamB
-   in (Day3 teamMatch singles1 doubles1 singles2 doubles2 singles3, g''''')
+makeDay3 :: (RandomGen g) => Team -> Team -> State g Day3
+makeDay3 teamA teamB = do
+  shuffledA <- shuffleTeam teamA
+  shuffledB <- shuffleTeam teamB
+  let teamMatch = TeamMatchup (shuffledA, shuffledB)
+  singles1 <- singlesMatchup teamA teamB
+  (doubles1, singles2, doubles2) <- exhaustiveMatchSeq teamA teamB
+  singles3 <- singlesMatchup teamA teamB
+  pure $ Day3 teamMatch singles1 doubles1 singles2 doubles2 singles3
 
-makeDay4 :: (RandomGen g) => g -> Team -> Team -> (Day4, g)
-makeDay4 gen teamA teamB =
-  let (Team a1 a2 a3 a4 a5, g') = shuffleTeam gen teamA
-      (Team b1 b2 b3 b4 b5, g'') = shuffleTeam g' teamB
-      (captainsPick, g''') = singlesMatchup g'' teamA teamB
-   in ( Day4
-          (SinglesMatchup (a1, b1))
-          (SinglesMatchup (a2, b2))
-          (SinglesMatchup (a3, b3))
-          (SinglesMatchup (a4, b4))
-          (SinglesMatchup (a5, b5))
-          captainsPick,
-        g'''
-      )
+makeDay4 :: (RandomGen g) => Team -> Team -> State g Day4
+makeDay4 teamA teamB = do
+  Team a1 a2 a3 a4 a5 <- shuffleTeam teamA
+  Team b1 b2 b3 b4 b5 <- shuffleTeam teamB
+  captainsPick <- singlesMatchup teamA teamB
+  pure $
+    Day4
+      (SinglesMatchup (a1, b1))
+      (SinglesMatchup (a2, b2))
+      (SinglesMatchup (a3, b3))
+      (SinglesMatchup (a4, b4))
+      (SinglesMatchup (a5, b5))
+      captainsPick
 
-makeSchedule :: (RandomGen g) => g -> Team -> Team -> Schedule
-makeSchedule gen teamA teamB =
-  let (day1, g') = makeDay1 gen teamA teamB
-      (day2, g'') = makeDay2 g' teamA teamB
-      (day3, g''') = makeDay3 g'' teamA teamB
-      (day4, _) = makeDay4 g''' teamA teamB
-   in Schedule day1 day2 day3 day4
+makeSchedule :: (RandomGen g) => Team -> Team -> State g Schedule
+makeSchedule teamA teamB = do
+  day1 <- makeDay1 teamA teamB
+  day2 <- makeDay2 teamA teamB
+  day3 <- makeDay3 teamA teamB
+  day4 <- makeDay4 teamA teamB
+  pure $ Schedule day1 day2 day3 day4
+
+scheduleToList :: Schedule -> [Matchup]
+scheduleToList
+  ( Schedule
+      (Day1 m1 m2 m3 m4)
+      (Day2 m5 m6 m7 m8 m9)
+      (Day3 m10 m11 m12 m13 m14 m15)
+      (Day4 m16 m17 m18 m19 m20 m21)
+    ) =
+    [ m1,
+      m2,
+      m3,
+      m4,
+      m5,
+      m6,
+      m7,
+      m8,
+      m9,
+      m10,
+      m11,
+      m12,
+      m13,
+      m14,
+      m15,
+      m16,
+      m17,
+      m18,
+      m19,
+      m20,
+      m21
+    ]
--- src/mosconi-sim/Sim.hs
+++ src/mosconi-sim/Sim.hs
@@ -1,12 +1,122 @@
-module Sim where
-import System.Random (RandomGen)
-import Match (Team)
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
+
+module Sim (runMosconi, MosconiTeam (..), losingTeamMatchesWon) where
+
+import Control.Monad.Trans.State.Lazy (State, state)
+import Match
+  ( DoublesTeam (..),
+    Matchup (..),
+    Player (..),
+    Schedule,
+    Team,
+    scheduleToList,
+    teamToList,
+  )
+import System.Random (RandomGen, randoms, split)
 
 data MosconiTeam = USA Team | Europe Team deriving (Eq, Show)
 
--- this would be an ADT from the other module, so i can just put all the
--- matchups in a list and `go` until I have a winner
-data Matchup
+data MosconiResult = MosconiResult
+  { winner :: MosconiTeam,
+    losingTeamMatchesWon :: Int
+  }
+  deriving (Show)
 
-pickWinner :: RandomGen g => g -> Matchup -> MosconiTeam
-pickWinner gen matchup = undefined
+data SimState = SimState
+  { aWins :: Int,
+    bWins :: Int,
+    teamA :: MosconiTeam,
+    teamB :: MosconiTeam
+  }
+
+data PlayerRecord = PlayerRecord
+  { singlesWins :: Int,
+    singlesLosses :: Int,
+    doublesWins :: Int,
+    doublesLosses :: Int
+  }
+  deriving (Show)
+
+newtype MatchupResult = MatchupResult
+  {winningTeam :: MosconiTeam}
+
+initSimState :: MosconiTeam -> MosconiTeam -> SimState
+initSimState = SimState 0 0
+
+winProbabilityForRatings :: (Floating a) => a -> a -> a
+winProbabilityForRatings aRating bRating =
+  1 / (1 + 10 ** ((bRating - aRating) / 400))
+
+-- |
+-- Calculate the win probability for a matchup. For singles matchups,
+-- FargoRate says:
+--
+--  "When two players are 100 points apart, say a 300 versus a 400,
+--  the ratio of game wins will be near 1:2, as in 5 games to 10 games, or 50 games to 100 games."
+--
+-- That's _pretty close_ to win probability with a scaling factor of 400 on Wikipedia's Elo page,
+-- so I'm assuming I can use that as the FargoRate scaling factor.
+--
+-- I don't really know how to interpret pairs / sets of ratings. Is the rating for a team of 2 players
+-- the arithmetic mean of the two players' ratings? Maybe! I don't know.
+--
+-- This function returns a list of probabilities, abusing laziness to make the probabilities work for
+-- the five player team match.
+perRackWinProbability :: Matchup -> [Double]
+perRackWinProbability
+  (SinglesMatchup (Player {rating = aRating}, Player {rating = bRating})) =
+    repeat $ winProbabilityForRatings (fromIntegral aRating) (fromIntegral bRating)
+perRackWinProbability (DoublesMatchup (DoublesTeam a1 a2, DoublesTeam b1 b2)) =
+  let aRating = fromIntegral (rating a1 + rating a2) / 2
+      bRating = fromIntegral (rating b1 + rating b2) / 2
+   in repeat $ winProbabilityForRatings aRating bRating
+-- reasonable test case -- a team matchup with identical ratings
+-- has the same probability sequences as a singles / doubles matchup with identical ratings
+perRackWinProbability (TeamMatchup (teamA, teamB)) =
+  let aRatings = fromIntegral . rating <$> teamToList teamA
+      bRatings = fromIntegral . rating <$> teamToList teamB
+   in cycle $ zipWith winProbabilityForRatings aRatings bRatings
+
+pickWinner :: (RandomGen g) => MosconiTeam -> MosconiTeam -> Matchup -> State g MatchupResult
+pickWinner a b matchup = state $ \g ->
+  let (genForThisMatch, genForRest) = split g
+   in go genForRest (initSimState a b) (zip (perRackWinProbability matchup) (randoms genForThisMatch))
+  where
+    -- this is impossible, since the function is called with two infinite lists, but oh well
+    go nextGenerator _ [] = (MatchupResult a, nextGenerator)
+    go nextGenerator matchupState ((aWinProbability, dieRoll) : xs) =
+      let nextState =
+            if dieRoll < aWinProbability
+              then
+                matchupState {aWins = aWins matchupState + 1}
+              else matchupState {bWins = bWins matchupState + 1}
+          winner =
+            if bWins nextState == 5
+              then Just (teamB nextState)
+              else if aWins nextState == 5 then Just (teamA nextState) else Nothing
+       in case winner of
+            Just t ->
+              if t == a
+                then
+                  (MatchupResult t, nextGenerator)
+                else (MatchupResult t, nextGenerator)
+            Nothing ->
+              go nextGenerator nextState xs
+
+-- this is the go call with evaluating each match and deciding whether to stop/go
+runMosconi :: (RandomGen g) => MosconiTeam -> MosconiTeam -> Schedule -> State g MosconiResult
+runMosconi a b schedule =
+  go (initSimState a b) (scheduleToList schedule)
+  where
+    go (SimState {aWins = 11, teamA, bWins}) _ = pure $ MosconiResult teamA bWins
+    go (SimState {bWins = 11, teamB, aWins}) _ = pure $ MosconiResult teamB aWins
+    -- also impossible I think? again, oh well
+    go (SimState {teamA, teamB, aWins, bWins}) [] =
+      let (winner', loserMatchupWins) = if aWins > bWins then (teamA, bWins) else (teamB, aWins)
+       in pure $ MosconiResult winner' loserMatchupWins
+    go (SimState {teamA, teamB, aWins, bWins}) (matchup : matchups) =
+      do
+        MatchupResult {winningTeam} <- pickWinner teamA teamB matchup
+        let nextState = if winningTeam == teamA then (SimState {aWins = aWins + 1, bWins, teamA, teamB}) else (SimState {bWins = bWins + 1, aWins, teamA, teamB})
+        go nextState matchups
