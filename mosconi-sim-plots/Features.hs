f193c6738cadacc59521d2a1e111cb759169bb7a
James Santucci <james.santucci@gmail.com>
2026-02-24 19:24:12 -0800

    WIP

--- mosconi-sim-plots/Features.hs
+++ mosconi-sim-plots/Features.hs
@@ -1,26 +1,49 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
 
-module Features (usaRacksWon) where
+module Features (usaRacksWonSeries, usaRacksWonCount) where
 
 import Data.Foldable (foldMap')
-import qualified DataFrame as DataFrame
-import Results (MosconiResult(..), MosconiTeam(..), ScheduleSummary (..), SimSummary (..))
+import DataFrame ((|>))
+import qualified DataFrame as D
+import DataFrame.Functions (col)
+import qualified DataFrame.Functions as F
+import qualified DataFrame.Operations.Aggregation as Agg
+import DataFrame.Operators ((.=))
+import Results (MosconiResult (..), MosconiTeam (..), ScheduleSummary (..), SimSummary (..))
 
-usaRacksWon :: SimSummary -> DataFrame.DataFrame
-usaRacksWon (SimSummary {summaries}) =
-  DataFrame.fromNamedColumns $
-    [ ( "Team USA Racks Won",
-        DataFrame.fromList $
-          foldMap'
-            ( \(ScheduleSummary {results}) ->
-                ( \case
-                    MosconiResult {winner = Europe _, losingTeamMatchesWon} -> losingTeamMatchesWon
-                    MosconiResult {} -> 11
-                )
-                  <$> results
+
+usaRacksWonSeries :: SimSummary -> D.DataFrame
+usaRacksWonSeries (SimSummary {summaries}) =
+  let colName = "Team USA Racks Won"
+   in ( D.fromNamedColumns $
+          [ ( colName,
+              D.fromList $
+                foldMap'
+                  ( \(ScheduleSummary {results}) ->
+                      ( \case
+                          MosconiResult {winner = Europe _, losingTeamMatchesWon} -> losingTeamMatchesWon
+                          MosconiResult {} -> 11
+                      )
+                        <$> results
+                  )
+                  summaries
             )
-            summaries
+          ]
       )
-    ]
+
+usaRacksWonCount :: D.DataFrame -> D.DataFrame
+usaRacksWonCount df =
+  let colName = "Team USA Racks Won"
+      -- type application enables solving for the appropriate type of racksWonCol,
+      -- which is important in the aggregate below
+      racksWonCol = col @Int colName
+   in df
+        -- groupBy column because first column
+        |> Agg.groupBy [colName]
+        -- Text becomes name of aggregation result
+        |> Agg.aggregate ["count" .= F.count racksWonCol]
+        |> D.sortBy [D.Asc racksWonCol]
--- mosconi-sim-plots/Main.hs
+++ mosconi-sim-plots/Main.hs
@@ -4,16 +4,23 @@
 module Main where
 
 import Data.Aeson (eitherDecodeFileStrict)
-import Features
-import DataFrame.Display.Web.Plot as Plt
 import qualified Data.Text.IO as T
+import DataFrame.Display.Web.Plot (defaultPlotConfig)
+import qualified DataFrame.Display.Web.Plot as Plt
+import Features
 
 -- TODO:
 -- [x] read in the json
--- make a dataframe out of the results with USA racks won as a feature (probably just want bars of this)
+-- [x] make a dataframe out of the results with USA racks won as a feature (probably just want bars of this)
+-- [ ] accept argument for which summary json to read
 main :: IO ()
-main = do
-  Right summary <- eitherDecodeFileStrict "big.json"
-  let usaRacks = usaRacksWon summary
-  HtmlPlot plot <- Plt.plotSingleBars "Team USA Racks Won" (defaultPlotConfig Bar) usaRacks
-  T.writeFile "bars.html" plot
+main =
+  let barPlotConfig = (defaultPlotConfig Plt.Bar)
+   in do
+        Right summary <- eitherDecodeFileStrict "wnt-ranked.json"
+        let usaRacks = usaRacksWonSeries summary
+        let usaRackCounts = usaRacksWonCount usaRacks
+        Plt.HtmlPlot linePlot <-
+          Plt.plotLinesWith "Team USA Racks Won" ["count"] barPlotConfig usaRackCounts
+        -- (Plt.defaultPlotConfig Plt.Line)
+        T.writeFile "lines-wnt.html" linePlot
