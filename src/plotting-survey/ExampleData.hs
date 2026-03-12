4a70f37469e71bbaefa9e76d6b39d7346f3854c7
James Santucci <james.santucci@gmail.com>
2026-03-12 13:40:02 -0700

    Add configured scatter plots
    * titles and fonts
    * axis labels and limits
    * plot size
    * color cycles

--- src/plotting-survey/ExampleData.hs
+++ src/plotting-survey/ExampleData.hs
@@ -1,14 +1,20 @@
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
 
 module ExampleData where
 
 import Control.Monad (replicateM)
 import Control.Monad.Trans.State (StateT, state)
 import qualified DataFrame as D
-import System.Random (Random (random), RandomGen)
+import qualified DataFrame.Typed as DT
+import System.Random (RandomGen, random, randomR)
 
-points :: (RandomGen g, Monad m) => Int -> StateT g m [Float]
+points :: (RandomGen g, Monad m) => Int -> StateT g m [Double]
 points n = replicateM n . state $ random
+
+labels :: (RandomGen g, Monad m) => Int -> StateT g m [Char]
+labels n = replicateM n . state $ randomR ('a', 'j')
 
 pointsDf :: (RandomGen g, Monad m) => Int -> StateT g m D.DataFrame
 pointsDf size = do
@@ -19,3 +25,18 @@ pointsDf size = do
       [ ("x", D.fromList x),
         ("y", D.fromList y)
       ]
+
+type LabeledDfSchema = '[DT.Column "x" Double, DT.Column "y" Double, DT.Column "tag" Char]
+
+labeledPointsDf :: (RandomGen g, Monad m) => Int -> StateT g m (DT.TypedDataFrame LabeledDfSchema)
+labeledPointsDf size = do
+  x <- points size
+  y <- points size
+  tag <- labels size
+  pure $
+    DT.unsafeFreeze @LabeledDfSchema $
+      D.fromNamedColumns
+        [ ("x", D.fromList x),
+          ("y", D.fromList y),
+          ("tag", D.fromList tag)
+        ]
